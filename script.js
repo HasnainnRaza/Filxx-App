4a813e06a8a3a36c4668129013f0ba7afda9b448
HasnainnRaza <info.hasnainrazaaa@gmail.com>
2024-07-26 17:28:19 +0500

    Shows details made

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ async function displayPopularShows() {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-          <a href="movie-details.html?id=${show.id}">
+          <a href="tv-details.html?id=${show.id}">
             ${
                 show.poster_path
                 ? `<img
@@ -137,6 +137,66 @@ async function displayMovieDetails() {
         document.querySelector('#movie-details').appendChild(div);
   }
 
+  // Display Show Details 
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+  console.log(showId);
+
+   const show = await fetchAPIData(`tv/${showId}`);
+
+     // Overlay for background image
+     displayBackgroundImage('tv', show.backdrop_path);
+
+     const div = document.createElement('div');
+
+     div.innerHTML = `<div class ="details-top">
+         <div>
+         ${
+           show.poster_path
+           ? `<img
+         src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+         class="card-img-top"
+         alt="${show.name}"
+       />` : `<img
+         src="../images/no-image.jpg"
+         class="card-img-top"
+         alt="${show.name}"
+       />`
+       }
+         </div>
+         <div>
+           <h2>${show.name}</h2>
+           <p>
+             <i class="fas fa-star text-primary"></i>
+             ${show.vote_average.toFixed(1)} / 10
+           </p>
+           <p class="text-muted">Last Air Date Date: ${show.last_air_date}</p>
+           <p>
+             ${show.overview}
+           </p>
+           <h5>Genres</h5>
+           <ul class="list-group">
+             ${show.genres.map((genre) => `<li>${genre.name} </li>`).join('')}
+           </ul>
+           <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+         </div>
+       </div>
+       <div class="details-bottom">
+         <h2>Show Info</h2>
+         <ul>
+           <li><span class="text-secondary">Number Of Episodes :</span> ${show.number_of_episodes}
+             </li>
+           <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air} </li>
+           <li><span class="text-secondary">Status:</span> ${show.status}</li>
+         </ul>
+         <h4>Production Companies</h4>
+         <div class="list-group">${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+         </div>
+       </div>`;
+
+       document.querySelector('#show-details').appendChild(div);
+ }
+
   // Display Backdrop On Details Pages
   function displayBackgroundImage(type, backgroundPath) {
       const overlayDiv = document.createElement('div');
@@ -205,7 +265,7 @@ function init() {
     } else if (global.currentPage === '/movie-details.html') {
         displayMovieDetails();
     } else if (global.currentPage === '/tv-details.html') {
-        console.log('Tv-Details');
+        displayShowDetails();
     } else if (global.currentPage === '/search.html') {
         console.log('Search Page');
     }
